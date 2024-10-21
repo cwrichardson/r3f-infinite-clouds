b17d7032a6b71a9cf2081d371f591062fa82f7f2
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-10-21 17:39:57 -0400

    Start using clouds for texture.

--- src/components/instancedMesh.js
+++ src/components/instancedMesh.js
@@ -6,10 +6,25 @@ import { DoubleSide, PlaneGeometry } from 'three';
 
 import { vertex } from '@/glsl/vertex';
 import { fragment } from '@/glsl/fragment';
+import { useTexture } from '@react-three/drei';
 
 export const InstancedMesh = forwardRef((props, ref) => {
     const { instanceLocations, instanceRotations } = props;
     const shaderRef = useRef();
+
+    /*
+     * Playing around with different textures for the clouds. In the
+     * tutorial, Yuri had actually grabbed the sprite from the site, but
+     * the site is no longer live, so we don't have access to that.
+     * 
+     * cumulus2 looks best to my eye for a single cloude, but we'll see
+     * if we end up using more than one texture.
+     */
+    const brush = useTexture('/images/brush.png');
+    const cumulusRound = useTexture('/images/cloud-5992791_640.png')
+    const cumulus = useTexture('/images/cloud-2421760_640.png')
+    const cumulus2 = useTexture('/images/clouds-7081496_640.png')
+    const stratus = useTexture('/images/cloud-159388_640.png')
 
     useFrame((state, delta, xrFrame) => {
         // do animation
@@ -55,12 +70,14 @@ export const InstancedMesh = forwardRef((props, ref) => {
                 ref={shaderRef}
                 extensions={{ derivatives: "#extension GL_OES_standard_derivatives : enable"}}
                 uniforms={{
+                    uTexture: { value: cumulus2 },
                     uTime: { value: 0 }
                 }}
                 side={DoubleSide}
                 vertexShader={vertex}
                 fragmentShader={fragment}
                 depthTest={false}
+                depthWrite={false}
                 transparent
                 />
             </mesh>
--- src/glsl/fragment.js
+++ src/glsl/fragment.js
@@ -1,8 +1,12 @@
 export const fragment = /* glsl */ `
+    uniform sampler2D uTexture;
+
     varying float vAlpha;
     varying vec2 vUv;
 
     void main() {
-        gl_FragColor = vec4(vUv, 0., vAlpha);
+        vec4 map = texture2D(uTexture, vUv);
+        // gl_FragColor = vec4(vUv, 0., vAlpha);
+        gl_FragColor = map;
     }
 `;
